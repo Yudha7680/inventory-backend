9f175b5fdf1c33c7a62ff202daa99ef218c1ad44
Yudha7680 <yudha.napoleon@gmail.com>
2025-11-23 07:05:04 +0700

    Fix middleware path

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyToken } from "../middleware/auth.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 import pool from "../config/db.js";
 
 const router = express.Router();
